60ccaf239eec91bc2253cb80f8e6f756b6fb965a
Zhansaule Telisheva <jansaule97@mail.ru>
2023-02-12 21:42:49 +0500

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  let res = [];
+  let numStr = number.toString();
+  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number < 20) {
+    return ones[number];
+  }
+
+  if (number >= 20 && number < 100 && number % 10 == 0) {
+    return tens[number / 10];
+  }
+
+  if (numStr.length == 2) {
+    return tens[+numStr[0]] + ' ' + ones[+numStr[1]];
+  }
+
+  if (numStr.length == 3) {
+    if (numStr[1] == 0 && numStr[2] == 0) {
+      return ones[+numStr[0]] + " " + "hundred";
+    } else {
+      return ones[+numStr[0]] + " hundred " + toReadable(+(numStr[1] + numStr[2]));
+    }
+  }
+
+};
